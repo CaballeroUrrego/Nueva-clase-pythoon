7383bee23fe5a0e81f769138defa895dce9ac1af
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2025-06-02 15:35:51 -0500

    Actualizacion

--- funciones/07-ejercicios.py
+++ funciones/07-ejercicios.py
@@ -1,10 +1,22 @@
-def es_palindromo(texto):
-    # Eliminar espacios y convertir a minúsculas
-    texto_limpio = ''.join(texto.lower().split())
-    # Comprobar si es igual al revés
-    return texto_limpio == texto_limpio[::-1]
+def no_space(texto):
+    nuevo_texto = ""
+    for cha in texto:
+        if cha != " ":
+            nuevo_texto += cha
+    return nuevo_texto
 
-# Pruebas
+def reverse(texto):
+    texto_al_reves = ""
+    for cha in texto:
+        texto_al_reves = cha + texto_al_reves
+    return texto_al_reves
+
+def es_palindromo(texto):
+    texto = no_space(texto).lower()
+    texto_al_reves = reverse(texto)
+    return texto.lower() == texto_al_reves.lower()
+
+# Ejemplos de uso:
 print("abba", es_palindromo("abba"))
 print("Reconocer", es_palindromo("Reconocer"))
 print("Amo la paloma", es_palindromo("Amo la paloma"))
---
+++ tipos-avanzados/01-listas.py
@@ -0,0 +1,5 @@
+numeros = 	[1, 2, 3]
+letras = ['a', 'b', 'c']
+palabras = ['chachito', 'feliz']
+palabrasFelices = ['Gonorrea', 'contento', 'alegre']
+print(palabrasFelices[0]) 
